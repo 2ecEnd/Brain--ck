3dd289ace445ae55579af07075a0af9558da25ac
Several_SIZE <kner647@gmail.com>
2025-05-05 16:24:21 +0700

    added homePage

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.mobileapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mobileapp.ui.theme.MobileAppTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MobileAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    App(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,76 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App(modifier: Modifier = Modifier){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomePage(modifier, navController)
+        }
+        composable("projects") {
+            ProjectsPage(modifier, navController)
+        }
+    }
+}
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier, navController: NavController) {
+    Box(modifier = Modifier.background(Color.Green).fillMaxSize()){
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Button(
+                onClick = {navController.navigate("projects")},
+                modifier = Modifier.width(200.dp)
+            )
+            {
+                Text("Проекты")
+            }
+
+            val context = LocalContext.current
+            Button(
+                onClick = {
+                    val homeIntent = Intent(Intent.ACTION_MAIN).apply {
+                    addCategory(Intent.CATEGORY_HOME)
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    }
+                    context.startActivity(homeIntent)
+                },
+                modifier = Modifier.width(200.dp)
+            )
+            {
+                Text("Выйти")
+            }
+        }
+    }
+}
+
+@Composable
+fun ProjectsPage(modifier: Modifier = Modifier, navController: NavController){
+    Box(modifier = Modifier.background(Color.Green).fillMaxSize()){
+        Column(
+            modifier = Modifier.align(Alignment.Center),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Button(
+                onClick = {},
+                modifier = Modifier.width(200.dp)
+            )
+            {
+                Text("+")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobileAppTheme {
-        Greeting("Android")
-    }
+fun AppPreview() {
+    App()
 }
