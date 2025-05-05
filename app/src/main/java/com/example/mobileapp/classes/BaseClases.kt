6ab2ff7ce4645883c6fee26aac9cf8197c4e47ea
lee_alex <leealex.2006@mail.ru>
2025-05-05 14:43:52 +0700

    changed base classes properties

--- app/src/main/java/com/example/mobileapp/classes/BaseClases.kt
+++ app/src/main/java/com/example/mobileapp/classes/BaseClases.kt
@@ -4,7 +4,6 @@ import android.graphics.Color
 import androidx.compose.runtime.Composable
 
 abstract class BlockTemplate {
-    abstract val name: String // Название блока
     abstract val color: Color // Цвет блока (ui)
 
     abstract fun runCode() // Выполняет свою функцию
@@ -12,8 +11,7 @@ abstract class BlockTemplate {
     abstract fun drawBlock() // Отрисовывает блок (выставляет нужные параметры) (ui)
 }
 
-abstract class Block(blockName : String, blockColor: Color) : BlockTemplate() {
-    abstract override val name: String // Название блока
+abstract class Block() : BlockTemplate() {
     abstract override val color: Color // Цвет блока (ui)
 
     abstract override fun runCode() // Выполняет свою функцию
@@ -21,8 +19,7 @@ abstract class Block(blockName : String, blockColor: Color) : BlockTemplate() {
     abstract override fun drawBlock() // Отрисовывает блок (выставляет нужные параметры) (ui)
 }
 
-abstract class ComplexBlock(blockName : String, blockColor: Color) : BlockTemplate() {
-    abstract override val name: String // Название блока
+abstract class ComplexBlock() : BlockTemplate() {
     abstract override val color: Color // Цвет блока (ui)
     abstract var blockList: MutableList<BlockTemplate> // Список блоков, которые он будет содержать
     abstract var varList: MutableList<Block> //Список переменных, доступных в его области видимости
