45a26b5fe1f93057343035ec86bcad501721232c
lee_alex <leealex.2006@mail.ru>
2025-05-05 14:03:33 +0700

    added all bases classes

--- app/src/main/java/com/example/mobileapp/classes/BlockTemplate.kt
+++ app/src/main/java/com/example/mobileapp/classes/BlockTemplate.kt
@@ -2,13 +2,32 @@ package com.example.mobileapp.classes
 
 import android.graphics.Color
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
 
 abstract class BlockTemplate {
-    abstract val name : String;
-    abstract val color : Color;
+    abstract val name: String // Название блока
+    abstract val color: Color // Цвет блока (ui)
 
-    abstract fun runCode();
+    abstract fun runCode() // Выполняет свою функцию
     @Composable
-    abstract fun drawBlock();
+    abstract fun drawBlock() // Отрисовывает блок (выставляет нужные параметры) (ui)
+}
+
+abstract class Block(blockName : String, blockColor: Color) : BlockTemplate() {
+    abstract override val name: String // Название блока
+    abstract override val color: Color // Цвет блока (ui)
+
+    abstract override fun runCode() // Выполняет свою функцию
+    @Composable
+    abstract override fun drawBlock() // Отрисовывает блок (выставляет нужные параметры) (ui)
+}
+
+abstract class ComplexBlock(blockName : String, blockColor: Color) : BlockTemplate() {
+    abstract override val name: String // Название блока
+    abstract override val color: Color // Цвет блока (ui)
+    abstract var blockList: MutableList<BlockTemplate> // Список блоков, которые он будет содержать
+    abstract var varList: MutableList<Block> //Список переменных, доступных в его области видимости
+
+    abstract override fun runCode() // Выполняет свою функцию
+    @Composable
+    abstract override fun drawBlock() // Отрисовывает блок (выставляет нужные параметры) (ui)
 }
