bdf67621f00d5c9acb0254f0fce68de72c0a21b8
lee_alex <leealex.2006@mail.ru>
2025-05-12 21:57:43 +0700

    added class Context

---
+++ app/src/main/java/com/example/mobileapp/classes/Context.kt
@@ -0,0 +1,20 @@
+package com.example.mobileapp.classes
+
+import androidx.compose.ui.graphics.Color
+
+class Context : ComplexBlock() {
+    override val color: Color
+        get() = TODO("Not yet implemented")
+    override var blockList: MutableList<BlockTemplate>
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var varList: MutableMap<String, Any>
+        get() = TODO("Not yet implemented")
+        set(value) {}
+
+    override fun execute() {
+        for (i in 0..blockList.size){
+            blockList[i].execute()
+        }
+    }
+}
