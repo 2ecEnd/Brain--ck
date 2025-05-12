49276d8dfb363536d04558f523b98c1cb1a22714
Several_SIZE <kner647@gmail.com>
2025-05-13 00:25:46 +0700

    Added drag and drop system

--- app/src/main/java/com/example/mobileapp/Blocks.kt
+++ app/src/main/java/com/example/mobileapp/Blocks.kt
@@ -1,26 +1,92 @@
 package com.example.mobileapp
 
+import android.R
+import android.R.bool
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.gestures.drag
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import com.example.mobileapp.classes.BlockTemplate
+import kotlin.math.roundToInt
 
 @Composable
-fun DeclareVariableBlock(){
+fun DrawBlock(block: BlockTemplate?, onDragStart: (Offset) -> Unit, onDragEnd: (BlockTemplate?) -> Unit, isNeedToUpdateDropZone: Boolean, dropZoneUpdated: (Rect) -> Unit){
+    var density = LocalDensity.current
+
     Card(
         modifier = Modifier
             .width(200.dp)
             .height(48.dp)
-            .background(Color(red = 255, green = 128, blue = 0)),
-        shape = RoundedCornerShape(15.dp)
+            .padding(start = 16.dp)
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = { offset -> onDragStart(offset) },
+                    onDrag = { _, _ -> },
+                    onDragEnd = { onDragEnd(block) }
+                )
+            }
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(15.dp)
+            )
+            .onGloballyPositioned { coordinates ->
+                if(isNeedToUpdateDropZone) {
+                    with(density) {
+                        var position = coordinates.positionInWindow()
+                        var newZone = Rect(
+                            left = position.x,
+                            top = position.y + 48.dp.toPx(),
+                            right = position.x + 200.dp.toPx(),
+                            bottom = position.y + 48.dp.toPx() + 48.dp.toPx()
+                        )
+                        dropZoneUpdated(newZone)
+                    }
+                }
+            },
+        shape = RoundedCornerShape(15.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(255, 128, 0)),
     )
     {
 
     }
 }
+
+@Composable
+fun DrawShadow(block: BlockTemplate?){
+    Card(
+        modifier = Modifier
+            .width(184.dp)
+            .height(48.dp),
+        shape = RoundedCornerShape(15.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(25, 25, 25).copy(alpha = 0.25f)),
+    )
+    {
+
+    }
+}
+
--- app/src/main/java/com/example/mobileapp/Pages.kt
+++ app/src/main/java/com/example/mobileapp/Pages.kt
@@ -1,8 +1,10 @@
 package com.example.mobileapp
 
 import android.R.string
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
@@ -19,24 +22,40 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInWindow
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
+import com.example.mobileapp.classes.BlockTemplate
+import kotlin.math.roundToInt
 
 @Composable
 fun HomePage(navController: NavController) {
@@ -81,7 +100,9 @@ fun HomePage(navController: NavController) {
 
 @Composable
 fun ProjectsPage(navController: NavController){
-    Box(modifier = Modifier.fillMaxSize().background(Color.Green)){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Green)){
         Image(
             painter = painterResource(id = R.drawable.background),
             contentDescription = "background",
@@ -122,7 +143,9 @@ fun ProjectsPage(navController: NavController){
 
 @Composable
 fun CreateProjectPage(navController: NavController){
-    Box(modifier = Modifier.fillMaxSize().background(Color.Green)){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Green)){
         Image(
             painter = painterResource(id = R.drawable.background),
             contentDescription = "background",
@@ -178,7 +201,70 @@ fun ProjectNameForm(navController: NavController){
 
 @Composable
 fun RedactorPage(navController: NavController){
-    Box(modifier = Modifier.fillMaxSize().background(Color(red = 249, green = 249, blue = 249))){
+    var draggingBlock by remember { mutableStateOf<BlockTemplate?>(null) }
+    var isDragging by remember { mutableStateOf(false) }
+    var dragOffset by remember { mutableStateOf(Offset.Zero) }
+    var touchPoint by remember { mutableStateOf(Offset.Zero) }
+
+    var blockList = remember { mutableStateListOf<BlockTemplate?>() }
+    var dropZone by remember { mutableStateOf(Rect.Zero) }
+
+    fun AddNewBlock(block: BlockTemplate?){
+        if (dropZone.contains(Offset(dragOffset.x, dragOffset.y))){
+            blockList.add(block)
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(red = 249, green = 249, blue = 249))
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent()
+                        val position = event.changes.first().position
+                        if (event.type == PointerEventType.Move && isDragging) {
+                            dragOffset = position
+                        }
+                    }
+                }
+            }
+    )
+    {
+        if (isDragging){
+            Box(
+                modifier = Modifier
+                    .offset {
+                        IntOffset(
+                            (dragOffset.x - touchPoint.x).roundToInt(),
+                            (dragOffset.y - touchPoint.y).roundToInt()
+                        )
+                    }
+                    .zIndex(1f)
+            )
+            {
+                DrawBlock(null, {}, {}, false, {})
+            }
+
+            if(dropZone.contains(Offset(dragOffset.x, dragOffset.y))) {
+                Box(
+                    modifier = Modifier
+                        .offset {
+                            IntOffset(
+                                dropZone.left.roundToInt(),
+                                dropZone.top.roundToInt()
+                            )
+                        }
+                        .zIndex(0.95f)
+                )
+                {
+                    DrawShadow(null)
+                }
+            }
+        }
+
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -194,15 +280,7 @@ fun RedactorPage(navController: NavController){
                 HomeButton(navController)
             }
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.75f)
-                    .background(Color(red = 249, green = 249, blue = 249))
-            )
-            {
-
-            }
+            RedactorArea(blockList, {rect -> dropZone = rect})
 
             Card(
                 modifier = Modifier
@@ -219,13 +297,60 @@ fun RedactorPage(navController: NavController){
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .fillMaxHeight(0.33f)
+                            .fillMaxHeight(0.33f),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Start
                     )
                     {
-                        DeclareVariableBlock()
+                        DrawBlock(null, {offset ->
+                            isDragging = true
+                            touchPoint = offset },
+                            {block ->
+                                isDragging = false
+                                AddNewBlock(block)
+                            }, false, {}
+                        )
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun RedactorArea(blockList: SnapshotStateList<BlockTemplate?>, dropZoneUpdated: (Rect) -> Unit){
+    var density = LocalDensity.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.75f)
+            .background(Color(red = 249, green = 249, blue = 249))
+    )
+    {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(24.dp)
+                .padding(top = 16.dp)
+                .onGloballyPositioned { coordinates ->
+                    with(density){
+                        var position = coordinates.positionInWindow()
+                        var newZone = Rect(
+                            left = position.x + 12.dp.toPx(),
+                            top = position.y + 8.dp.toPx(),
+                            right = position.x + 200.dp.toPx(),
+                            bottom = position.y + 24.dp.toPx() + 48.dp.toPx()
+                        )
+                        dropZoneUpdated(newZone)
+                    }
+                },
+            colors = CardDefaults.cardColors(containerColor = Color(25, 25, 25)),
+        )
+        {}
+
+        blockList.forEach { block ->
+            DrawBlock(null, {}, {}, true, dropZoneUpdated)
         }
     }
 }
