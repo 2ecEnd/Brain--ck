9e4ac6888ab3e2dd243ab7083d610f532ee7bd86
lee_alex <leealex.2006@mail.ru>
2025-05-16 12:32:30 +0700

    MathExpression with many data types

--- app/src/main/java/com/example/mobileapp/classes/MathExpression.kt
+++ app/src/main/java/com/example/mobileapp/classes/MathExpression.kt
@@ -4,21 +4,82 @@ class MathExpression: Block()
 {
     var leftValue: BlockTemplate = Constant()
     var rightValue: BlockTemplate = Constant()
-    var operartion: String = "+"
+    var operation: String = "+"
 
     override fun execute(): Value
     {
-        var left = (leftValue.execute()) as Int
-        var right = (rightValue.execute()) as Int
+        val left = (leftValue.execute()) as Value
+        val right = (rightValue.execute()) as Value
 
-        return when(operartion)
+        return when
         {
-            "+" -> left + right
-            "-" -> left - right
-            "*" -> left * right
-            "/" -> left / right
-            "%" -> left % right
-            else -> throw IllegalArgumentException("Ошибка")
+            (left is Value.BOOLEAN || right is Value.BOOLEAN) ->
+            {
+                throw IllegalArgumentException("Логический тип данных недопустим для математического выражения")
+            }
+            (left is Value.STRING || right is Value.STRING) ->
+            {
+                val l = when(left)
+                {
+                    is Value.STRING -> left.value
+                    is Value.INT -> left.value.toString()
+                    is Value.DOUBLE -> left.value.toString()
+                    else -> throw IllegalArgumentException("Недопустимый тип данных для математического выражения")
+                }
+                val r = when(right)
+                {
+                    is Value.STRING -> right.value
+                    is Value.INT -> right.value.toString()
+                    is Value.DOUBLE -> right.value.toString()
+                    else -> throw IllegalArgumentException("Недопустимый тип данных для математического выражения")
+                }
+
+                when (operation)
+                {
+                    "+" -> Value.STRING(l + r)
+                    else -> throw IllegalArgumentException("Недопустимая операция для строк")
+                }
+            }
+            (left is Value.INT || left is Value.DOUBLE &&
+            right is Value.INT || right is Value.DOUBLE) ->
+            {
+
+                if (left is Value.INT && right is Value.INT)
+                {
+                    when (operation)
+                    {
+                        "+" -> Value.INT(left.value + right.value)
+                        "-" -> Value.INT(left.value - right.value)
+                        "*" -> Value.INT(left.value * right.value)
+                        "/" -> Value.INT(left.value / right.value)
+                        else -> throw IllegalArgumentException("Недопустимая операция")
+                    }
+                }
+                else
+                {
+                    val l = when (left)
+                    {
+                        is Value.INT -> left.value.toDouble()
+                        is Value.DOUBLE -> left.value
+                        else -> throw IllegalArgumentException("Недопустимый тип данных для математического выражения")
+                    }
+                    val r = when (right)
+                    {
+                        is Value.INT -> right.value.toDouble()
+                        is Value.DOUBLE -> right.value
+                        else -> throw IllegalArgumentException("Недопустимый тип данных для математического выражения")
+                    }
+                    when (operation)
+                    {
+                        "+" -> Value.DOUBLE(l + r)
+                        "-" -> Value.DOUBLE(l - r)
+                        "*" -> Value.DOUBLE(l * r)
+                        "/" -> Value.DOUBLE(l / r)
+                        else -> throw IllegalArgumentException("Недопустимая операция")
+                    }
+                }
+            }
+            else -> throw IllegalArgumentException("Недопустимый тип данных для математического выражения")
         }
     }
 }
