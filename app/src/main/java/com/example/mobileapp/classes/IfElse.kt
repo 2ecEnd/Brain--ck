da91f5d4ac92b6271b08f83fe700f3b5ddaf31eb
lee_alex <leealex.2006@mail.ru>
2025-05-17 17:51:48 +0700

    bugfix

--- app/src/main/java/com/example/mobileapp/classes/IfElse.kt
+++ app/src/main/java/com/example/mobileapp/classes/IfElse.kt
@@ -7,6 +7,14 @@ class IfElse: Block()
         override var blockList = mutableListOf<BlockTemplate>()
         override var varList = mutableMapOf<String, Value>()
 
+        override fun deleteVariable(name: String)
+        {
+            varList.remove(name)
+        }
+        override fun addVariable(name: String)
+        {
+            varList.put(name, Value.INT(0))
+        }
         override fun execute()
         {
             for (i in 0..<blockList.size)
@@ -21,6 +29,14 @@ class IfElse: Block()
         override var blockList = mutableListOf<BlockTemplate>()
         override var varList = mutableMapOf<String, Value>()
 
+        override fun deleteVariable(name: String)
+        {
+            varList.remove(name)
+        }
+        override fun addVariable(name: String)
+        {
+            varList.put(name, Value.INT(0))
+        }
         override fun execute() 
         {
             for (i in 0..<blockList.size)
