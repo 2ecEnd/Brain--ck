37cb2ee9b8d1eacc993856eb8379dc169c4b5fc5
lee_alex <leealex.2006@mail.ru>
2025-05-24 18:05:16 +0700

    added secondary constructor in Constant

--- app/src/main/java/com/example/mobileapp/classes/Constant.kt
+++ app/src/main/java/com/example/mobileapp/classes/Constant.kt
@@ -2,11 +2,46 @@ package com.example.mobileapp.classes
 
 import androidx.compose.ui.geometry.Rect
 
-class Constant : Block()
+class Constant(var value: Value = Value.INT(0)): Block()
 {
-    var value: Value = Value.INT(0)
     override var selfRect: Rect = Rect.Zero
     override var parent: BlockTemplate? = null
+
+    constructor(type: String, value_: Any) : this()
+    {
+        when (type)
+        {
+            "string" ->
+            {
+                if (value_ is String)
+                    value = Value.STRING(value_)
+                else
+                    throw IllegalArgumentException("Ошибка")
+            }
+            "int" ->
+            {
+                if (value_ is Int)
+                    value = Value.INT(value_)
+                else
+                    throw IllegalArgumentException("Ошибка")
+            }
+            "double" ->
+            {
+                if (value_ is Double)
+                    value = Value.DOUBLE(value_)
+                else
+                    throw IllegalArgumentException("Ошибка")
+            }
+            "bool" ->
+            {
+                if (value_ is Boolean)
+                    value = Value.BOOLEAN(value_)
+                else
+                    throw IllegalArgumentException("Ошибка")
+            }
+            else -> throw  IllegalArgumentException("Ошибка")
+        }
+    }
 
     fun setValue(type: String, value_: Any)
     {
@@ -40,7 +75,9 @@ class Constant : Block()
                 else
                     throw IllegalArgumentException("Ошибка")
             }
+            else -> throw  IllegalArgumentException("Ошибка")
         }
     }
+
     override fun execute(): Value = value
 }
