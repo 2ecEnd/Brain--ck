fcc394c31a312432356c9c3f260769f051e247c6
lee_alex <leealex.2006@mail.ru>
2025-05-27 20:10:05 +0700

    fix ListConstant

--- app/src/main/java/com/example/mobileapp/classes/ListConstant.kt
+++ app/src/main/java/com/example/mobileapp/classes/ListConstant.kt
@@ -2,14 +2,33 @@ package com.example.mobileapp.classes
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.geometry.Rect
+import com.example.mobileapp.R
 
 class ListConstant(
     override var scope: ComplexBlock,
     var value: Value.LIST = Value.LIST(mutableStateListOf())
 ) : Block()
 {
+    var blockList: MutableList<BlockTemplate> = mutableStateListOf()
     override var parent: BlockTemplate? = null
     override var selfRect: Rect = Rect.Zero
 
-    override fun execute() = value
+    override fun execute() : Value
+    {
+        for (block in blockList)
+        {
+            when (block)
+            {
+                is Constant -> value.value.add(block.execute())
+                is MathExpression -> value.value.add(block.execute())
+                is BoolExpression -> value.value.add(block.execute())
+                is UseVariable -> value.value.add(block.execute())
+                is UseListElement -> value.value.add(block.execute())
+                is ListConstant -> value.value.add(block.execute())
+                else -> throw Exception(R.string.unknown_error.toString())
+            }
+        }
+
+        return value
+    }
 }
